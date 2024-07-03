fd9447130c2b412ae4f4207fa5f946b1afa1b29e
Ayihan SRP <ayihan@srptechs.com>
2024-07-04 02:24:44 +0500

    feat: Add hash data to Retake Photo link in Generateimg.jsx and Uploadimg2.jsx

--- src/pages/generateimg/Generateimg.jsx
+++ src/pages/generateimg/Generateimg.jsx
@@ -14,7 +14,7 @@ const Generateimg = () => {
   return (
     <div className='upload-main'>
       <div className='upload-img'>
-          <Link to='/upimg2'><img className='back' src={Back} alt='top-icon'/></Link>
+          <Link to={`/upimg2#${data}`}><img className='back' src={Back} alt='top-icon'/></Link>
           <img src={Header} alt='top-icon'/>
       </div>
       <div className='genimg-boxtext'>
